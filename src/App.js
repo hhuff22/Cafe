6f4c19f71d7cd33e117b35103a96479f90e0c1b1
Herman Huff <hhuff22@gmail.com>
2022-03-29 10:13:44 -0400

    Remove unused images

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main'
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 import Sites from './components/Sites';
 import Careers from './components/Careers';
-import { HashRouter } from 'react-router-dom';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <HashRouter>
-      <Header />
-      <Switch>
-        <Route path='/index.html' component={Main} />
-        <Route path='/menu.html' component={Menu} />
-        <Route path='/sites.html' component={Sites} />
-        <Route path='/careers.html' component={Careers} />
-        <Redirect to='/index.html' />
-      </Switch>
-      <Footer />
-    </HashRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path='/index.html' component={Main} />
+          <Route path='/menu.html' component={Menu} />
+          <Route path='/sites.html' component={Sites} />
+          <Route path='/careers.html' component={Careers} />
+          <Redirect to='/index.html' />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
