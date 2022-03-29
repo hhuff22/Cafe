a927c8290b47acba2b65b70271a0129b606750f0
Herman Huff <hhuff22@gmail.com>
2022-03-28 23:46:33 -0400

    Add redirects

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Footer from './components/Footer';
 import Menu from './components/Menu';
 import Sites from './components/Sites';
 import Careers from './components/Careers';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Switch>
-        <Route path='/index' component={Main} />
+        <Route path='/index.html' component={Main} />
         <Route path='/menu.html' component={Menu} />
         <Route path='/sites.html' component={Sites} />
         <Route path='/careers.html' component={Careers} />
-        <Redirect to='/index' />
+        <Redirect to='/index.html' />
       </Switch>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
